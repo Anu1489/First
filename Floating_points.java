fd8dd04c4409a74ba92210b7f9c4d0a82b392329
Anu1489 <anudeveloper14@gmail.com>
2021-06-24 18:29:32 +0530

    updated new files

---
+++ Floating_points.java
@@ -0,0 +1,13 @@
+class Floating_points{
+	public static void main(String[] args){
+		double myDouble = 3.4;
+		float myFloat = 2.5F;
+
+		double MyDoubleScientific = 3.55e25;
+
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(MyDoubleScientific);
+
+	}
+}
